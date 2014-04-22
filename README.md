1608d16a5dc6097eb24f58c6744743129eaef563
Andrew <aj.cumine@btinternet.com>
2014-04-22 11:47:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Reddit Clone Project
+
+## Week 10 project at Makers Academy
+
+### Contributers
+
+Tiffanie Chia
+
+---
+
+Project to create a ruby on rails application including:
+
+* upvoting
+* downvoting
+
+---
+
+### Techologies used
+
+* Ruby
+* Ruby on Rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditClone::Application.config.secret_key_base = 'deff554bb816ecc09d0d410a6dc7b1a87720a889a529dd77d6346a12119802412f3e4eaca8e1a531e5cee8cf298e72b000d71ef95b159fd937b0bc3c78d5b5e6'
