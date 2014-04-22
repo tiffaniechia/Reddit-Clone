1d58effd0a98f90323bab508c1afb47d0b0d201c
Andrew <aj.cumine@btinternet.com>
2014-04-22 13:55:15 +0100

    first rspec tests

---
+++ spec/features/post_features_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'post index page' do
+  
+  context 'no posts' do
+    it 'shows a message' do
+      visit '/posts'
+      expect(page).to have_content('No posts yet')
+    end
+  end
+
+  context 'valid posts' do
+
+    before(:each) do
+      post = Post.create(comment: 'some comment')
+    end
+
+    it 'displays comments' do
+      visit '/posts'
+      expect(page).to have_content('some comment')
+    end
+  end
+
+  describe 'adding a post' do
+    it 'displays a new post' do
+      visit '/posts'
+      click_link('Add Post')
+      fill_in 'Comment', with: 'some new comment'
+      click_button('Create Post')
+      expect(current_path).to eq('/posts')
+      expect(page).to have_content('some new comment')
+    end
+  end
+
+end
